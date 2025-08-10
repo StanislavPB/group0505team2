f8062d1a9ad52df3bac80729e1dd398a9bccf2c8
StanislavPB <perminovsb@gmail.com>
2025-08-10 09:54:42 +0100

    Initial commit

---
+++ src/main/java/com/group0505team2/Group0505team2Application.java
@@ -0,0 +1,13 @@
+package com.group0505team2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group0505team2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Group0505team2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/group0505team2/Group0505team2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group0505team2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group0505team2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
